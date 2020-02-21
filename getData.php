398d753189fbe919d746cba9984fab120f96db71
felipej9805 <37278637+felipej9805@users.noreply.github.com>
2020-02-21 17:18:20 -0500

    Update getData.php

--- getData.php
+++ getData.php
@@ -4,7 +4,7 @@ $username = "demouser";
 $password = "demopassword";
 $dbname = "demodb";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Conexion fallida");
 $query = "SELECT * FROM personas";
 
 $result = mysqli_query($conn,$query); 
@@ -14,8 +14,8 @@ echo "HICE ALGO";
 //guardamos en un array multidimensional todos los datos de la consulta
 
 $i=0;
-file_put_contents("/var/www/html/response.txt", "");
-$myfile = fopen("/var/www/html/response.txt","w") or die ("Unable to open file!");
+file_put_contents("/home/vagrant/provision/response.txt", "");
+$myfile = fopen("/home/vagrant/provision/response.txt","w") or die ("Unable to open file!");
 while($row = mysqli_fetch_array($result))
 {	
 	fwrite($myfile, $row['id'] ."_".  $row['nombre']. "\n" );
